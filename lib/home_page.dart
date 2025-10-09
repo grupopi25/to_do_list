b8e7d0ba3c8042d82670d0ba821356a0e892b806
xxxx <xxxxxx>
2025-10-09 19:31:28 -0300

    criando e validando formulario

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _listaTarefa = [];
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +26,59 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 18),
+            child: Form(
+              key: _formKey,
+
+              child: TextFormField(
+                controller: _controller,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Informe uma Tarefa';
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Adicionar uma Tarefa',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: InkWell(
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  _listaTarefa.add(_controller.text);
+                  _controller.clear();
+                }
+              },
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(color: Colors.green),
+                child: Center(
+                  child: Text(
+                    'Enviar Tarefa',
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaTarefa.length,
+              itemBuilder: (context, index) {
+                return ListTile(title: Text(_listaTarefa[index]));
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
